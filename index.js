bb4d01908a03c5a03c8bc8ef95639855979e5fca
AChar770 <amandatcharles@yahoo.com>
2025-12-31 05:22:13 -0500

    Prompt and User Input array created

---
+++ index.js
@@ -0,0 +1,38 @@
+const userInput = prompt ("What flavor(s) would you like: Vanilla, Strawberry, Coffee", "Vanilla, Vanilla, Vanilla, Strawberry, Coffee, Coffee");
+
+const flavorChoices = userInput.split (",");
+console.table(flavorChoices);
+
+// this ^^^ function is for the flavors separated to be used as data
+
+// const flavorOption = userInput.split (",")
+console.log (flavorChoices)
+
+// function count (arr) {
+// const flavorMap = 1};
+// // start at index 0
+// // stop at the end of the array arr. length
+// // step one element/item at a time
+// for (let i = 0; i < arr. length; i++) R
+// const flavor = arrlil;
+// // if the flavor is in the flavorMap object add one to the value
+// if (flavor in flavorMap) {
+// flavorMap [flavor] += 1;
+// // otherwise assign it the value of 1.
+// } else 1
+// flavorMap [flavor] = 1;
+
+// return the flavorMap
+
+
+
+
+
+// const flavorCounts = {};
+0
+
+// for (let flavor of custOrder) {
+//     if (flavorChoices[custOrder]) 
+//         {flavorChoices[custOrder]++;
+
+//     }else {(flavorChoices[custOrder]){flavorChoices[custOrder] = 1;}}}
